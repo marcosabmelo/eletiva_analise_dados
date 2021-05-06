ea7d872106afb337e01b4ac290c9340b819ee11d
marcosabmelo <marcosmelo.uast@gmail.com>
2021-05-05 21:46:29 -0300

    Tentei rodar o código no final mas o Google agora pede uma API Key

--- projeto/mapa_acidentes.R
+++ projeto/mapa_acidentes.R
@@ -1,6 +1,15 @@
 # ---------------------------------------------------------------------------------------
+# Bibliotecas
+# ---------------------------------------------------------------------------------------
+#
+#
+# ---------------------------------------------------------------------------------------
 # carrega a base de snistros de transito do site da PCR
 #
+# Base 2016
+sinistrosRecife2016Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/2caa8f41-ccd9-4ea5-906d-f66017d6e107/download/acidentes_2016-jan.csv', sep = ';', encoding = 'UTF-8')
+# Base 2017
+sinistrosRecife2017Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/2caa8f41-ccd9-4ea5-906d-f66017d6e107/download/acidentes_2017-jan.csv', sep = ';', encoding = 'UTF-8')
 # Base 2018
 sinistrosRecife2018Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/2caa8f41-ccd9-4ea5-906d-f66017d6e107/download/acidentes_2018-jan.csv', sep = ';', encoding = 'UTF-8')
 # Base 2019
@@ -14,7 +23,9 @@ sinistrosRecife2021Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087
 # ---------------------------------------------------------------------------------------
 # junta as bases de dados com comando rbind (juntas por linhas)
 # 
-sinistrosRecifeRaw <- rbind(sinistrosRecife2018Raw,
+sinistrosRecifeRaw <- rbind(sinistrosRecife2016Raw,
+                            sinistrosRecife2017Raw,
+                            sinistrosRecife2018Raw,
                             sinistrosRecife2019Raw,
                             sinistrosRecife2020Raw,
                             sinistrosRecife2021Raw)
@@ -24,3 +35,47 @@ sinistrosRecifeRaw <- rbind(sinistrosRecife2018Raw,
 # observa a estrutura dos dados
 str(sinistrosRecifeRaw)
 # ---------------------------------------------------------------------------------------
+
+# exporta em formato nativo do R
+saveRDS(sinistrosRecifeRaw, "projeto/sinistrosRecife.rds")
+
+sinistrosRecife <- readRDS("projeto/sinistrosRecife.rds")
+
+
+
+
+
+
+# Geocoding a csv column of "addresses" in R
+
+#install.packages("ggmap")
+
+library(ggmap)
+
+# Select the file from the file chooser
+#fileToLoad <- file.choose(new = TRUE)
+
+# Read in the CSV data and store it in a variable 
+#origAddress <- read.csv(fileToLoad, stringsAsFactors = FALSE)
+origAddress <- sinistrosRecife
+
+
+# Initialize the data frame
+geocoded <- data.frame(stringsAsFactors = FALSE)
+
+# Loop through the addresses to get the latitude and longitude of each address and add it to the
+# origAddress data frame in new columns lat and lon
+for(i in 1:nrow(origAddress))
+{
+  # Print("Working...")
+  result <- geocode(origAddress$endereco[i], output = "latlona", source = "google")
+  origAddress$lon[i] <- as.numeric(result[1])
+  origAddress$lat[i] <- as.numeric(result[2])
+  origAddress$geoAddress[i] <- as.character(result[3])
+}
+# Write a CSV file containing origAddress to the working directory
+write.csv(origAddress, "geocoded.csv", row.names=FALSE)
+
+
+
+
